fa5dc49335b72006e8f77f3c26e0bcbb210eb8b4
Rupali1310 <113013294+Rupali1310@users.noreply.github.com>
2023-07-04 15:07:51 +0530

    homepage code added

--- src/pages/homepage.java
+++ src/pages/homepage.java
@@ -2,9 +2,14 @@ package pages;
 
 public class homepage {
 
+		public void home()
+		{
+		System.out.println("home method");
+		}
 		public static void main(String[] args)
 		{
 		System.out.println("Homepage");
+		
 		}
 
 		
